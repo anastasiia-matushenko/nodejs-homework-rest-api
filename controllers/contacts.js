6cdc0d599d492a3bfaab2524ed0562c56b6928b7
anastasiia-matushenko <muzaam07@gmail.com>
2023-01-05 14:06:23 +0200

    added db

---
+++ controllers/contacts.js
@@ -0,0 +1,63 @@
+const { HttpError, ctrlWrapper } = require("../helpers");
+const { Contact } = require("../models/contact");
+
+const getContactsAll = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const updateContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const removeContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json({ message: "contact deleted" });
+};
+
+module.exports = {
+  getContactsAll: ctrlWrapper(getContactsAll),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  updateContactById: ctrlWrapper(updateContactById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+  removeContactById: ctrlWrapper(removeContactById),
+};
